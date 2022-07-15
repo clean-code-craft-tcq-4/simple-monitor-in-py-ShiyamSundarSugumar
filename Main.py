f279b88b7aacda64a94739d71311a9cde0e2599b
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-07-15 16:35:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,43 @@
-from Battery_check import battery_is_ok
+from battery_check import battery_is_ok
+
+
+
+class Battery:
+      
+      def __init__(self) -> None:
+          super().__init__()
+          self.Language = "En"
+          self.temp_min = 0
+          self.temp_max = 45
+          self.soc_min = 20
+          self.soc_max = 80
+          self.charge_rate_min = None
+          self.charge_rate_max = 0.8
+
+
+      def battery_check_warning_assert(self):
+          
+          #warning Assert
+          assert(battery_is_ok(44, 50, 0.8,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is True)
+          assert(battery_is_ok(25, 78, 0.8,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is True)
+          assert(battery_is_ok(25, 50, 0.79,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is True)
+
+      def battery_check_Error_assert(self):
+                  
+          #Error Assert
+          assert(battery_is_ok(25, 70, 0.7,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is True)
+          assert(battery_is_ok(50, 90, 0.9,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(50, 80, 0.5,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(40, 90, 0.5,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(40, 75, 0.9,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(55, 90, 0.5,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(55, 70, 1.0,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          assert(battery_is_ok(40, 90, 0.9,self.temp_min,self.temp_max,self.soc_min,self.soc_max,self.charge_rate_min,self.charge_rate_max) is False)
+          
+                  
+
 
 if __name__ == '__main__':
-
-  temp_min = 0
-  temp_max = 45
-  soc_min = 20
-  soc_max = 80
-  charge_rate_min = None
-  charge_rate_max = 0.8
-
-
-  assert(battery_is_ok(25, 70, 0.7,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is True)
-  assert(battery_is_ok(50, 90, 0.9,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(50, 80, 0.5,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(40, 90, 0.5,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(40, 75, 0.9,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(55, 90, 0.5,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(55, 70, 1.0,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  assert(battery_is_ok(40, 90, 0.9,temp_min,temp_max,soc_min,soc_max,charge_rate_min,charge_rate_max) is False)
-  
+      bat = Battery()
+      bat.battery_check_warning_assert()
+      bat.battery_check_Error_assert()

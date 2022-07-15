1ab068256cfd2ea8a60b8bf1b90ce0babd4d1d48
ShiyamSundarSugumar <105203939+ShiyamSundarSugumar@users.noreply.github.com>
2022-07-15 16:50:55 +0530

    Update check_warning_limit.py

--- check_warning_limit.py
+++ check_warning_limit.py
@@ -27,8 +27,33 @@ def check_warning_max_only(value,max_low_range,max_high_range):
         return True
     return False
 
+def warning_check_function(value,min_low_range,min_high_range,max_low_range,max_high_range):
+    if(min_low_range == None):
+        return check_warning_max_only(value,max_low_range,max_high_range)
+    else:
+       return check_warning_range(value,min_low_range,min_high_range,max_low_range,max_high_range)
+
+def check_warning_max_only(value,max_low_range,max_high_range):
+    if(max_low_range<=value) and (value < max_high_range):
+        return True
+    return False
+
 def check_warning_range(value,min_low_range,min_high_range,max_low_range,max_high_range):
-    if(  ( (min_low_range< value) and (value <=min_high_range)) or ((max_low_range<= value) and (value < max_high_range)) ):
+    
+    max_feedback = check_warning_max(value,max_low_range,max_high_range)
+    min_feedback = check_warning_min(value,min_low_range,min_high_range)
+    if(max_feedback and min_feedback):
+        return True
+    return False
+
+def check_warning_max(value,max_low_range,max_high_range):
+    if(((max_low_range<= value) and (value < max_high_range))):
+        return True
+    return False
+
+
+def check_warning_min(value,min_low_range,min_high_range):
+    if(((min_low_range< value) and (value <=min_high_range))):
         return True
     return False
 
